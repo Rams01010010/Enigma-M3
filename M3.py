140e97d2b29605a8d9f286cfab5507ae842dd41d
Rams01010010 <74339634+Rams01010010@users.noreply.github.com>
2021-02-03 16:42:38 +0530

    Add files via upload

--- M3.py
+++ M3.py
@@ -1,15 +1,3 @@
-# Details
-ETW = '''ABCDEFGHIJKLMNOPQRSTUVWXYZ'''
-RI = ['''ABCDEFGHIJKLMNOPQRSTUVWXYZ''','''EKMFLGDQVZNTOWYHXUSPAIBRCJ''','''ABCDEFGHIJKLMNOPQRSTUVWXYZ''']
-RII = ['''ABCDEFGHIJKLMNOPQRSTUVWXYZ''','''AJDKSIRUXBLHWTMCQGZNPYFVOE''','''ABCDEFGHIJKLMNOPQRSTUVWXYZ''']
-RIII = ['''ABCDEFGHIJKLMNOPQRSTUVWXYZ''','''BDFHJLCPRTXVZNYEIWGAKMUSQO''','''ABCDEFGHIJKLMNOPQRSTUVWXYZ''']
-RIV = ['''ABCDEFGHIJKLMNOPQRSTUVWXYZ''','''ESOVPZJAYQUIRHXLNFTGKDCMWB''','''ABCDEFGHIJKLMNOPQRSTUVWXYZ''']
-RV = ['''ABCDEFGHIJKLMNOPQRSTUVWXYZ''','''VZBRGITYUPSDNHLXAWMJQOFECK''','''ABCDEFGHIJKLMNOPQRSTUVWXYZ''']
-RB = '''YRUHQSLDPXNGOKMIEBFZCWVJAT'''
-RC = '''FVPJIAOYEDRZXWGCTKUQSBNMHL'''
-rotors = {'I':RI,"II":RII,"III":RIII,"IV":RIV,"V":RV,"B":RB,"C":RC}
-notch  = {'I':'Q',"II":'E',"III":'V',"IV":'J',"V":'Z'}
-
 def setRotVal(value):
     if value == 'I':
         return ['''ABCDEFGHIJKLMNOPQRSTUVWXYZ''','''EKMFLGDQVZNTOWYHXUSPAIBRCJ''','''ABCDEFGHIJKLMNOPQRSTUVWXYZ''']
@@ -21,7 +9,16 @@ def setRotVal(value):
         return ['''ABCDEFGHIJKLMNOPQRSTUVWXYZ''','''ESOVPZJAYQUIRHXLNFTGKDCMWB''','''ABCDEFGHIJKLMNOPQRSTUVWXYZ''']
     elif value == 'V':
         return ['''ABCDEFGHIJKLMNOPQRSTUVWXYZ''','''VZBRGITYUPSDNHLXAWMJQOFECK''','''ABCDEFGHIJKLMNOPQRSTUVWXYZ''']
+    elif value == 'B':
+        return '''YRUHQSLDPXNGOKMIEBFZCWVJAT'''
+    elif value == 'C':
+        return '''FVPJIAOYEDRZXWGCTKUQSBNMHL'''
 #--------------------------------------------------------------------------------------
+
+# Details
+ETW = '''ABCDEFGHIJKLMNOPQRSTUVWXYZ'''
+rotors = {'I':setRotVal('I'),"II":setRotVal('II'),"III":setRotVal('III'),"IV":setRotVal('IV'),"V":setRotVal('V'),"B":setRotVal('B'),"C":setRotVal('C')}
+notch  = {'I':'Q',"II":'E',"III":'V',"IV":'J',"V":'Z'}
 
 class M3:
     Plugboard = dict()
@@ -31,10 +28,10 @@ class M3:
     #setter
     def setter(self,RI='I',RII='II',RIII='III',rI='A',rII='A',rIII='A',plugboard='',reflector='B',iI='A',iII='A',iIII='A'):
         self.R1,self.R2,self.R3,self.sr1,self.sr2,self.sr3,self.r1,self.r2,self.r3,self.plugBoard,self.reflector,self.i1,self.i2,self.i3 = setRotVal(RI),setRotVal(RII),setRotVal(RIII),setRotVal(RI),setRotVal(RII),setRotVal(RIII),rI,rII,rIII,plugboard,rotors[reflector],iI,iII,iIII
-        self.S = [self.sr1,self.sr2,self.sr3]
-        self.R = [self.R1,self.R2,self.R3]
-        self.r = [self.r1,self.r2,self.r3]
-        self.i = [self.i1,self.i2,self.i3]
+        self.S = ['',self.sr1,self.sr2,self.sr3]
+        self.R = [self.reflector,self.R1,self.R2,self.R3]
+        self.r = ['',self.r1,self.r2,self.r3]
+        self.i = ['',self.i1,self.i2,self.i3]
 
         self.Plugboard = {'A':'A', 'B':'B', 'C':'C', 'D':'D', 'E':'E', 'F':'F', 'G':'G', 'H':'H', 'I':'I', 'J':'J', 'K':'K', 'L':'L', 'M':'M', 'N':'N', 'O':'O', 'P':'P', 'Q':'Q', 'R':'R', 'S':'S', 'T':'T', 'U':'U', 'V':'V', 'W':'W', 'X':'X', 'Y':'Y', 'Z':'Z'}
 
@@ -54,11 +51,11 @@ class M3:
                 exit()
         
         #set_ringsettings
-        for i in range(3):
+        for i in range(1,4):
             self.R[i][2] = self.rotate(self.R[i][2],self.R[i][2].index(self.r[i]))
 
         #set_initialSettings
-        for j in range(3):
+        for j in range(1,4):
             self.R[j][0] = self.rotate(self.R[j][0],self.R[j][2].index(self.i[j]))
             self.R[j][1] = self.rotate(self.R[j][1],self.R[j][2].index(self.i[j]))
             self.R[j][2] = self.rotate(self.R[j][2],self.R[j][2].index(self.i[j]))
@@ -70,7 +67,7 @@ class M3:
     
     #rotorPositions
     def getRotorPosition(self):
-        valuePack = (self.R[0][2][0],self.R[1][2][0],self.R[2][2][0])
+        valuePack = (self.R[1][2][0],self.R[2][2][0],self.R[3][2][0])
         return valuePack
 
     #get_keys from values
@@ -84,54 +81,29 @@ class M3:
         v = val[amt:]+val[:amt]
         return v
 
-    #Rotor3
-    def rotor3(self,val,isinp):
-        if isinp == True:
-            x = self.R[2][1][val]
-            matchIndex = self.R[2][0].index(x)
-            return self.rotor2(matchIndex,True)
-        else:
-            x = self.R[2][0][val]
-            matchIndex = self.R[2][1].index(x)
-            return self.Plugboard[ETW[matchIndex]] 
-
-    #Rotor2
-    def rotor2(self,val,isinp):
-        if isinp == True:
-            x = self.R[1][1][val]
-            matchIndex = self.R[1][0].index(x)
-            return self.rotor1(matchIndex,True)
-        else:
-            x = self.R[1][0][val]
-            matchIndex = self.R[1][1].index(x)
-            return self.rotor3(matchIndex,False)
-
-    #Rotor1
-    def rotor1(self,val,isinp):
-        if isinp == True:
-            x = self.R[0][1][val]
-            matchIndex = self.R[0][0].index(x)
-            return self.reflect(matchIndex)
-        else:
-            x = self.R[0][0][val]
-            matchIndex = self.R[0][1].index(x)
-            return self.rotor2(matchIndex,False)
-
-    #Reflector
-    def reflect(self,val):
-        x = self.reflector[val]
-        return self.rotor1(ETW.index(x),False)
+    #run_the_given_letter_through_rotors
+    def run_rotor(self,no,val):
+        if no == 0 :
+            x = self.reflector[val]
+            return ETW.index(x)
+        else: 
+            x = self.R[no][1][val]
+            matchIndex = self.R[no][0].index(x)
+            val = self.run_rotor(no-1,matchIndex)
+            x = self.R[no][0][val]
+            matchIndex = self.R[no][1].index(x)
+            return matchIndex
 
     #mainFunction
     def startMachine(self,Value):
-        self.autoRotate(2)
-        encryptedMessage = self.rotor3(ETW.index(self.Plugboard[Value]),True)
+        self.autoRotate(3)
+        encryptedMessage = self.Plugboard[ETW[self.run_rotor(3,ETW.index(self.Plugboard[Value]))]]
         return encryptedMessage
 
     #autoRotate
     def autoRotate(self,val):
         k = False
-        for i in range(2,0,-1):
+        for i in range(3,1,-1):
             if self.R[i][2][0] == notch[self.getKeys(rotors,self.S[i])]:
                 k = True
         if k:
@@ -146,7 +118,7 @@ class M3:
 if __name__ == '__main__':
     r = M3()
     r.setter('III','II','V','J','S','C','QH WP ES AY DX RC TF VB ZG UJ NM IK OL','B','C','S','E')
-    x = input("Enter a message : ")
+    x = input("Enter a message : ").upper()
     s = ''
     for i in x:
         s += r.startMachine(i)
